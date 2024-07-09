92cd2e5219b94fcfd3dca824afd2301a13deb67e
Burak <brkkcygt32@gmail.com>
2024-07-09 14:06:55 +0300

    initial

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,22 @@
 import "@styles/globals.css";
 
-const layout = () => {
-  return <div>layout</div>;
+export const metadata = {
+  title: "Promptopia",
+  description: "Discover & Share AI Prompts",
 };
 
-export default layout;
+const RootLayout = ({ children }) => {
+  return (
+    <html>
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+
+        <main className="app">{children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
---
+++ components/Nax.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Nax = () => {
+  return <div>Nax</div>;
+};
+
+export default Nax;
