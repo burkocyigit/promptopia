6dd4c53e9cb429ecd93d1b985bb6a4eaddaf2478
Burak <brkkcygt32@gmail.com>
2024-07-15 20:38:11 +0300

    Google authentication and mongodb connection

--- components/Provider.jsx
+++ components/Provider.jsx
@@ -1,7 +1,9 @@
-const Provider = () => {
-  return (
-    <div>Provider</div>
-  )
-}
+"use client";
 
-export default Provider
+import { SessionProvider } from "next-auth/react";
+
+const Provider = ({ children, session }) => {
+  return <SessionProvider session={session}> {children} </SessionProvider>;
+};
+
+export default Provider;
